44dd000645409ad6e9c7265ce9ea8fe2d6abd350
odan <odan3240@gmail.com>
2020-11-09 18:26:44 +0900

    add typecheck

---
+++ components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ count }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export const a = 10
+export default Vue.extend({
+  data: () => ({ count: 0 }),
+  methods: {
+    add (x: string) {
+      this.count += x
+    }
+  }
+})
+</script>
